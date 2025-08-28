f4b0c2cf30b8b66fd4ca4cbbdaba71c8fe48d26b
Nomad745 <2300749569@qq.com>
2025-08-28 13:21:44 +0800

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Supabase 项目配置（来自你的提供）
-export const SUPABASE_URL = 'https://rqrunzhckcjgngntpjkj.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxcnVuemhja2NqZ25nbnRwamtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMDcxMTUsImV4cCI6MjA3MTc4MzExNX0.Wp95SDUqG6ZEBbi_wKqUysrgJgZ54deV8r41Lf_OlOE';
